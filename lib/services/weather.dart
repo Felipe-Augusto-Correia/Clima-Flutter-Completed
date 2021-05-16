0b07f48c382a6db10694c733303165bdaaba1600
Felipe Augusto <51971892+Felipe-Augusto-Correia@users.noreply.github.com>
2021-05-16 18:06:53 -0300

    first commit

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,7 +1,7 @@
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
 
-const apiKey = 'e72ca729af228beabd5d20e3b7749713';
+const apiKey = '3d36a89c523648fc3fa467279c978f54';
 const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
 
 class WeatherModel {
